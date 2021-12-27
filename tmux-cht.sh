0787ea4c4b9d3e83505fd5ace556c8c67398eb11
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2021-12-27 12:37:01 +0330

    adding tmux sessionizer and tmux-ch.sh

---
+++ tmux-cht.sh
@@ -0,0 +1,14 @@
+
+
+
+
+selected=`cat ~/.tmux-cht-languages ~/.tmux-cht-command | fzf`
+read -p "enter query: " query
+
+if grep -qs "$selected" ~/.tmux-cht-languages; then
+	query=`echo $query | tr ' ' '+'`
+	tmux neww bash -c "curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+else
+	tmux neww bash -c "curl cht.sh/$selected~$query & while [ : ]; do sleep 1; done"
+fi
+
