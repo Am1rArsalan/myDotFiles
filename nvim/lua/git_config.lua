938ea54a62703b38e677997677e53d0e14b97c44
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-09-03 09:54:11 +0430

    chore: update config

--- nvim/lua/git_config.lua
+++ nvim/lua/git_config.lua
@@ -4,13 +4,14 @@ local status, git = pcall(require, "git")
 if (not status) then return end
 
 git.setup({
+  signcolumn = false,  -- Toggle with `:Gitsigns toggle_signs`
   default_mappings = false, -- NOTE: `quit_blame` and `blame_commit` are still merged to the keymaps even if `default_mappings = false`
   keymaps = {
     -- Open blame window
     blame = "<Leader>gb",
     -- Open file/folder in git repository
     browse = "<Leader>go",
-  }
+  },
 })
 
 -- neo git
--- nvim/lua/my_telescope.lua
+++ nvim/lua/my_telescope.lua
@@ -31,6 +31,7 @@ telescope.setup {
           -- your custom normal mode mappings
           ["N"] = fb_actions.create,
           ["h"] = fb_actions.goto_parent_dir,
+          ["X"] = fb_actions.remove, 
           ["/"] = function()
             vim.cmd('startinsert')
           end
