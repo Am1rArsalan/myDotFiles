4a91cc38120ec46163ee6ce3a60dfb673fb9aa6c
4m1z <amirahmadzadehdev@gmail.com>
2026-09-11 16:44:13 +0200

    update conf

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -32,10 +32,10 @@ lsp.setup_nvim_cmp({
 lsp.set_preferences({
     suggest_lsp_servers = false,
     sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
+        error = '',
+        warn = '',
+        hint = '',
+        info = ''
     }
 })
 
